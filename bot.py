c9424c561a38c40521b17992023ee97eec6465f3
MilanMalta <113123937+MilanMalta@users.noreply.github.com>
2022-09-13 16:44:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-#import os
+import os
 
-#import config vars from heroku (like username and password)
-#username = os.environ['username']
-#password = os.environ['password']
+import config vars from heroku (like username and password)
+username = os.environ['milanmalta999@gmail.com']
+password = os.environ['Jarkoqw123?']
 
 print(42)
